dfa7c0bd7a9ac7adcde247ee740deed58aaab4d7
Helson Matos <helson.11@hotmail.com>
2022-03-12 19:50:53 -0300

    Create app react

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Layout>
+        <div>
+          <img source="https://avatars.githubusercontent.com/u/77506662?v=4" alt="Avatar of user"/>
+          <h1>Helson Matos</h1>
+          <h3>Uername:</h3>
+          <span>helsonmatos</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starreds</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>5</span>
+            </div>
+          </div>          
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  )
 }
-
 export default App;
